9a470b1372b987f6e98bc0f14d4c7c405f7e4409
Jacek Kwiecień <jacek.kwiecien@schibsted.pl>
2017-09-15 20:43:49 +0200

    camera1 attempt

--- things/src/main/java/net/techbrewery/jackie/MainActivity.kt
+++ things/src/main/java/net/techbrewery/jackie/MainActivity.kt
@@ -1,7 +1,13 @@
 package net.techbrewery.jackie
 
 import android.app.Activity
+import android.hardware.Camera
 import android.os.Bundle
+import android.util.Log
+import android.view.SurfaceHolder
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+
 
 /**
  * Skeleton of an Android Things activity.
@@ -25,8 +31,83 @@ import android.os.Bundle
  */
 class MainActivity : Activity() {
 
+    lateinit var surfaceViewHolder: SurfaceHolder
+    var camera: Camera? = null
+    var localSizes: List<Camera.Size>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        surfaceViewHolder = surfaceView.holder
+        surfaceViewHolder.addCallback(object : SurfaceHolder.Callback {
+            override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun surfaceDestroyed(p0: SurfaceHolder?) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun surfaceCreated(p0: SurfaceHolder?) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
+        surfaceViewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS)
+
+        val cameraId = findCamera()
+        cameraId?.let {
+            safeCameraOpen(cameraId)
+            startCameraPreview()
+        }
+
     }
+
+    private fun findCamera(): Int? {
+        val numberOfCameras = Camera.getNumberOfCameras()
+        for (i in 0 until numberOfCameras) {
+            val info = Camera.CameraInfo()
+            Camera.getCameraInfo(i, info)
+            return i
+        }
+        return null
+    }
+
+    private fun safeCameraOpen(id: Int): Boolean {
+        var qOpened = false
+
+        try {
+            releaseCameraAndPreview()
+            camera = Camera.open(id)
+            qOpened = camera != null
+        } catch (e: Exception) {
+            Log.e(getString(R.string.app_name), "failed to open Camera")
+            e.printStackTrace()
+        }
+
+        return qOpened
+    }
+
+    private fun startCameraPreview() {
+//        val localSizes = camera.parameters.supportedPreviewSizes
+//        mSupportedPreviewSizes = localSizes
+//        requestLayout()
+
+        try {
+            camera?.setPreviewDisplay(surfaceViewHolder)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        // Important: Call startPreview() to start updating the preview
+        // surface. Preview must be started before you can take a picture.
+        camera?.startPreview()
+    }
+
+    private fun releaseCameraAndPreview() {
+//        mPreview.setCamera(null)
+        camera?.release()
+        camera = null
+    }
+
 }
